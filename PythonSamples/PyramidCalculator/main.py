64a6acb59fde98d1ff75949c67d0120e82c6c169
IltonS <ilton.sequeira.s@gmail.com>
2021-03-22 17:39:01 -0300

    Adiciona a pasta PythonSamples

---
+++ PythonSamples/PyramidCalculator/main.py
@@ -0,0 +1,19 @@
+﻿def main():
+    menu = -1
+    
+    print("Bem-Vindo(a) a Calculadora de Pirâmides")
+    
+    while menu != 0:
+          print("\nSelecione uma opção:\n")
+          print("    [1] Calcular área da base")
+          print("    [2] Calcular largura da base")
+          print("    [3] Calcular comprimento da base")
+          print("    [4] Calcular altura")
+          print("    [5] Calcular volume")
+          print("    [0] Sair\n")
+          menu = int(input(">"))
+    
+    return 0
+
+if __name__ == "__main__":
+   main()
